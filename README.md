e9433571d7f4684c6d897a820c3c5d067cda22f9
duanxiaojie <18435179812@163.com>
2022-11-07 16:51:46 +0800

    yogi-cli

--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# yogi-cli
-★打造属于自己的脚手架工具-skr~
+<!--
+ * @Author: yogi
+ * @Date: 2022-10-27 16:52:27
+ * @LastEditors: yogi
+ * @LastEditTime: 2022-11-07 16:30:34
+ * @Description: Nothing Impossible
+-->
+## yogi-cli
+
+### 打造自己的脚手架工具，实现思路
+
+- 项目模板放在github/gitlab上
+- 用户通过命令交互的方式下载不同的模板
+- 经过模板引擎渲染定制项目模板
+- 模板变动，只需要更新模板即可，不需要用户更新脚手架
+
+****
+### 涉及知识点及模块
+
+- NodeJs
+  
+  基于Node.js开发命令行工具
+
+- ECMAScript 6
+
+  使用最新版本语言进行开发
+
+- npm 发包
+
+  npm包的发布及更新流程 
+  
+  ```『 <npm login> 登录 <npm publish> 发布 』```
+
+  登录失败E403 试试执行 
+
+  ```『 npm config set registry https://registry.npmjs.org 』```
+
+  全局下载yogi-cli失败可以试试
+
+  ```『 npm config set registry=https://registry.npm.taobao.org 』```
+
+  全局更新工具包
+
+  ```『 npm update <name> -g 』```
+
+- commander.js
+
+  可以自动的解析命令和参数，用于处理用户输入的命令
+
+- download-git-repo
+
+  下载并提取git仓库，用于下载项目模板
+
+- Inquirer.js
+  
+  通用的命令行用户界面集合，用于和用户进行交互
+
+- handlebars.js
+
+  模板引擎，将用户提交的信息动态填充到文件中
+
+- ora
+
+  下载过程久的话。可以用于显示下载中的动画效果
+
+- chalk
+
+  可以给终端的字体加上颜色
+
+- log-symbols
+
+  可以在终端上显示出√|×等提示图标
+
+****
+### 全局安装
+```
+npm install yogi-cli -g
+```
+
+### 初始化项目
+```
+yogi init <projectName>
+```
---
+++ bin/index.js
@@ -0,0 +1,89 @@
+#!/usr/bin/env node
+// 使用Node开发命令行工具所执行的Javascript脚本必须在顶部加入 #!/usr/bin/env node
+
+const ora = require('ora');
+const chalk = require('chalk');
+const program = require('commander');
+const logSymbols = require('log-symbols');
+const version = require("../package.json").version;
+const latestVersion = require("latest-version");
+const {
+  mkdirByProjectName,
+  choiceTemplate,
+  downloadByGit,
+  installModules,
+} = require("../lib/create");
+const { nodeProjectQuestion } = require("../lib/questions");
+
+program
+  .command('init <project-template>')
+  .description('「 初始化项目模板 」')
+  .action((templateName) => {
+    console.log(
+      chalk.greenBright(`
+          ┏┓　    ┏┓
+        ┏┛┻━━━┛┻┓
+        ┃            ┃
+        ┃     -      ┃
+        ┃  ＞   ＜   ┃
+        ┃　          ┃
+        ┃ ... 々 ...  ┃
+        ┃　          ┃
+        ┗━┓　    ┏━┛
+            ┃　    ┃
+            ┃　    ┃  神兽保佑
+            ┃　    ┃  永无BUG
+            ┃　    ┗━━━┓
+            ┃　           ┣┓⌒ 
+            ┃　           ┏┛  ⌒ ︶
+            ┗┓┓┏━┳┓┏┛
+              ┃┫┫　┃┫┫
+              ┗┻┛　┗┻┛
+      `)
+    );
+    console.log();
+    cliStart(templateName);
+  })
+
+program.on('--help', function () {
+  console.log();
+  console.log(chalk.green('创建项目：yogi init <projectName>'));
+  console.log();
+});
+
+// process.argv原生获取命令行参数的方式
+program.version(version, '-v, --version').parse(process.argv);
+if (program.args.length < 2) program.help();
+
+async function cliStart(templateName) {
+  /** 1. 版本检测 */
+  const spiner = ora(`${logSymbols.warning}  「 请稍等，版本检测中... 」`).start()
+  spiner.color = "yellow";
+  const onLineVersion = await latestVersion("yogi-cli");
+  spiner.stop();
+  if (version !== onLineVersion) {
+    console.log(
+      chalk.yellow(
+        "⚠️  请使用: < npm install -g yogi-cli@latest > 或 <npm update yogi-cli -g> 更新升级后再创建项目"
+      )
+    );
+    console.log();
+  } else {
+    console.log(chalk.green(`✅ 项目名称：<${templateName}>`));
+
+    /** 2. 检测创建的项目目录是否存在 */
+    await mkdirByProjectName();
+
+    /** 3. 选择对应的模板 */
+    const { choice } = await choiceTemplate();
+
+    /** 4. 执行用户信息输入 */
+    let answers = await nodeProjectQuestion();
+
+    /** 5. 下载模板 */
+    await downloadByGit(choice, answers);
+
+    /** 6. 安装依赖 */
+    await installModules();
+  }
+}
